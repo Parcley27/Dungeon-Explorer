2847bfe11f8419794fe0888d4727e61dc65cf32a
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2025-04-02 12:56:41 -0700

    feat: main core class

---
+++ DungeonExplorer.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class DungeonExplorer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+        
+        System.out.println("Welcome to Dungeon Explorer!");
+        System.out.println("Type 'help' to see available commands.");
+
+        while (isRunning) {
+            System.out.print("\n> ");
+            
+            String command = scanner.nextLine().toLowerCase();
+
+            switch (command) {
+                case "help":
+                    printHelp();
+
+                    break;
+                
+                case "explore":
+                    System.out.println("Run explore function");
+
+                    break;
+
+                case "attack":
+                    System.out.println("Run attack function");
+
+                    break;
+
+                case "quit":
+                    System.out.println("Thank you for playing, goodbye!");
+                    isRunning = false;
+
+                    break;
+            }
+        }
+    }
+
+    public static void printHelp() {
+        System.out.println("Available Commands: ");
+        System.out.println("- attack   : Attack an enemy if present");
+        System.out.println("- explore  : Move into a new room");
+        System.out.println("- help     : Shows command help menu");
+        System.out.println("- quit     : End the game");
+
+    }
+}
