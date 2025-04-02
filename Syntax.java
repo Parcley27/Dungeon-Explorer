194259a1cfac034025ccf9178eccc55b19c602d9
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2025-04-02 11:09:06 -0700

    feat: java operators

---
+++ Syntax.java
@@ -0,0 +1,40 @@
+public class Syntax {
+    
+    public static void main(String[] args) {
+        // Primitive types
+        int age = 17;
+        double gpa = 4.0;
+        boolean isStudent = true;
+        char initial = 'P';
+    
+        // Non primative types
+        String name = "Pierce"; // Class based
+
+        System.out.println("Name: " + name);
+        System.out.println("Initial: " + initial);
+        System.out.println("Age: " + age);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("GPA: " + gpa);
+        
+        // Operators
+        int a = 10, b = 3;
+        System.out.println(a + b); // 13
+        System.out.println(a - b); // 7
+        System.out.println(a * b); // 30
+        System.out.println(a / b); // 3 (integer division)
+        System.out.println(a % b); // 1 (mod, remainder)
+
+        // Comparison
+        System.out.println(a >= b); // True
+        System.out.println(a == b); // False
+        System.out.println(a != b); // True
+
+        // Logical
+        boolean isRed = true, isBlue = false;
+        System.out.println(isRed && isBlue); // False (and)
+        System.out.println(isRed || isBlue); // True (or)
+        System.out.println(!isRed); // False (is NOT red)
+
+
+    }
+}
