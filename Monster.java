c9c8f01a506bae7fd1cbc03c7c36a94a3851a155
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2025-04-02 22:02:25 -0700

    feat: add monster class

---
+++ Monster.java
@@ -0,0 +1,43 @@
+public class Monster {
+    private String name;
+    private int health, attackPower;
+
+    // Monster constructor
+    public Monster(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+
+    }
+
+    public void printStats() {
+        System.out.println("Moster: " + name);
+        System.out.println("Health: " + health);
+        System.out.println("Attack Power: " + attackPower);
+
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public int getHealth() {
+        return health;
+
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+
+        if (health < 0) health = 0;
+
+        System.out.println(name + " takes " + damage + "damage. Health: " + health);
+
+    }
+}
