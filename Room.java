a436686b57bc0a7d1ca249717a7ecc1a684d396b
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2025-04-03 14:17:53 -0700

    feat: add room class

---
+++ Room.java
@@ -0,0 +1,46 @@
+public class Room {
+    private String name;
+    private String description;
+    private Monster monster;
+
+    // Constructor
+    public Room(String name, String description, Monster monster) {
+        this.name = name;
+        this.description = description;
+        this.monster = monster;
+
+    }
+
+    public void printRoomDetails() {
+        System.out.println(name + ": " + description);
+
+        if (monster != null) {
+            System.out.println("A " + monster.getName() + " with " + monster.getHealth() + " is here!");
+
+
+        } else {
+            System.out.println("This room is empty of monsters.");
+
+        }
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public String getDescription() {
+        return description;
+        
+    }
+
+    public Monster getMoster() {
+        return monster;
+
+    }
+
+    public void clearMoster() {
+        monster = null;
+
+    }
+}
